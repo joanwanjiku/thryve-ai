5a9f948cc531b4c7853d717615d4222996d37994
joanwanjiku <joanevans18@gmail.com>
2025-12-18 17:48:09 -0500

    tested and confirmed creation, fetch , deletion and updating of progress metrics by user, by type in the progress controller class

---
+++ backend/src/main/java/com/thryveai/backend/controllers/ProgressController.java
@@ -0,0 +1,76 @@
+package com.thryveai.backend.controllers;
+
+import com.thryveai.backend.dto.ApiResponse;
+import com.thryveai.backend.dto.ProgressMetricDTO.ProgressMetricResponse;
+import com.thryveai.backend.dto.ProgressMetricDTO.CreateProgressMetricRequest;
+import com.thryveai.backend.entity.MetricType;
+import com.thryveai.backend.services.ProgressService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/v1/progress")
+@RequiredArgsConstructor
+public class ProgressController {
+    private final ProgressService progressService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<ProgressMetricResponse>> createProgressMetric(
+            @RequestHeader ("X-User-Id") UUID userId,
+            @Valid @RequestBody CreateProgressMetricRequest request
+    ) {
+        ProgressMetricResponse response = progressService.recordProgress(userId, request);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(ApiResponse.created(response));
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<Page<ProgressMetricResponse>>> getUserProgressMetrics(
+            @RequestHeader("X-User-Id") UUID userId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ){
+        Pageable pageable = PageRequest.of(page, size);
+        Page<ProgressMetricResponse> metrics = progressService.getUserProgress(userId, pageable);
+        return ResponseEntity.ok(ApiResponse.success(metrics));
+    }
+
+    @GetMapping("/type/{type}")
+    public ResponseEntity<ApiResponse<List<ProgressMetricResponse>>> getProgressMetricByType(
+            @RequestHeader("X-User-Id") UUID userId,
+            @PathVariable String type
+    ){
+        List<ProgressMetricResponse> response = progressService.getProgressByType(userId, MetricType.valueOf(type));
+        return ResponseEntity.ok(ApiResponse.success(response));
+    }
+
+    @GetMapping("/latest/{type}")
+    public ResponseEntity<ApiResponse<ProgressMetricResponse>> getLatestProgressMetricByType(
+            @RequestHeader("X-User-Id") UUID userId,
+            @PathVariable String type
+    ){
+        ProgressMetricResponse response = progressService.getLatestMetric(userId, MetricType.valueOf(type));
+        return ResponseEntity.ok(ApiResponse.success(response));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProgressMetric(
+            @PathVariable UUID id
+    ){
+        progressService.deleteProgress(id);
+        return ResponseEntity.ok().build();
+    }
+
+
+
+}
